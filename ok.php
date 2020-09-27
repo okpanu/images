31248b06a1aed7946e8b575664b64238f0e96a9f
okpanu <71703260+okpanu@users.noreply.github.com>
2020-09-27 15:32:57 +0530

    Create ok.php

---
+++ ok.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+ echo "<h1>Hello, PHP!</h1>\n";
+?>
+<img src="https://cdn77-pic.xvideos-cdn.com/videos/thumbs169ll/f9/83/e2/f983e2a825160055282d5a0884fdb584/f983e2a825160055282d5a0884fdb584.6.jpg" alt="Image created by a PHP script" width="200" 
+height="80">
+</body>
+</html>
